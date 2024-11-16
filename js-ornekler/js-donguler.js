fcc298db3204bc4cafa17b2974bc71c08f08249e
Yigit-Y34 <yigityesiloglu.is@gmail.com>
2024-11-16 20:32:47 +0300

    Add files via upload

---
+++ js-ornekler/js-donguler.js
@@ -0,0 +1,114 @@
+/*for (let i = 1;i <= 10;i++){
+    console.log(i);
+}
+
+const meyveler = ["Elma","Armut","Muz"];
+for(let i = 0;i < meyveler.length;i++){
+    console.log(meyveler[i]);
+}
+
+let i = 1;
+while( i<= 5){
+    console.log(i);
+    i++;
+}
+
+let sayi;
+do{sayi = parseInt(prompt("Pozitif bir sayı girin:"));}
+while(sayi <= 0);
+console.log("Teşekkürler!Pozitif bir sayı girdiniz:",sayi);
+
+const ogrenci = {
+    ad : "Ali",
+    yas : 20,
+    bolum : "Bilgisayar Mühendisliği"
+};
+
+for(let ozellik in ogrenci){
+    console.log(`${ozellik}:${ogrenci[ozellik]}`);
+}
+
+for(let i=1;i<=10;i++){
+    if(i===5){
+        break;
+    }
+    console.log(i);
+}
+
+for(let i=1;i<=10;i++){
+    if(i===5){
+        continue;
+    }
+    console.log(i);
+}
+
+for(let i=1;i<=5;i++){
+    for(let j=1;j<=5;j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+}
+
+let faktoriyel = 1;
+
+for (let i = 1; i <= 5; i++) {
+  faktoriyel *= i;
+}
+
+console.log(faktoriyel);
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+
+const meyveler = ["Elma","Armut","Muz"];
+for(let i = 0;i < meyveler.length;i++){
+    console.log(meyveler[i]);
+}
+
+for(let i = 1;i <= 5;i++){
+    for(let j = 1;j <= 5;j++){
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+}
+
+const ogrenci = {ad:"Ahmet",yas:21,bolum:"Bilgisayar Mühendisliği"};
+for(let ozellik in ogrenci){
+    console.log(`${ozellik}:${ogrenci[ozellik]}`)
+}
+
+const sayilar = [10,20,30,40];
+toplam = 0;
+for(let i=0;i<sayilar.length;i++){
+    toplam+=sayilar[i]
+}
+console.log(toplam);
+
+let rast;
+do{
+    rast = Math.floor(Math.random()*20);
+    console.log(`${rast}`)
+}
+while(rast<10);
+
+for(sayi = 2;sayi<=20;sayi++){
+    let asalmi= true;
+
+    for(let i = 2;i<sayi;i++){
+        if(sayi % i == 0){
+            asalmi = false;
+            break;
+        }
+    }
+
+    if(asalmi){
+        console.log(sayi);
+    }
+}
+
+const satirSayisi = 10;
+
+for(let i = 1;i<=satirSayisi;i++){
+    console.log(" ".repeat(satirSayisi-i) + "*".repeat(2*i-1));
+}*/
+
+  
